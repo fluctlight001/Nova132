7250b68c3f4e464d2f47d751a8002bcaa1527ec7
fluctlight001 <609804988@qq.com>
2021-11-20 13:53:01 +0800

    add : and andi

--- ID.v
+++ ID.v
@@ -231,7 +231,8 @@ module ID(
 
     // rs to reg1
     assign sel_alu_src1[0] = inst_add | inst_addiu | inst_addu | inst_subu | inst_ori | inst_or | inst_sw | inst_lw 
-                           | inst_xor | inst_sltu | inst_slt | inst_slti | inst_sltiu | inst_addi | inst_sub;
+                           | inst_xor | inst_sltu | inst_slt | inst_slti | inst_sltiu | inst_addi | inst_sub 
+                           | inst_and | inst_andi;
 
     // pc to reg1
     assign sel_alu_src1[1] = inst_jal;
@@ -242,10 +243,10 @@ module ID(
     
     // rt to reg2
     assign sel_alu_src2[0] = inst_add | inst_addu | inst_subu | inst_sll | inst_or | inst_xor | inst_sltu | inst_slt
-                           | inst_sub;
+                           | inst_sub | inst_and;
     
     // imm_sign_extend to reg2
-    assign sel_alu_src2[1] = inst_lui | inst_addiu | inst_sw | inst_lw | inst_slti | inst_sltiu | inst_addi;
+    assign sel_alu_src2[1] = inst_lui | inst_addiu | inst_sw | inst_lw | inst_slti | inst_sltiu | inst_addi | inst_andi;
 
     // 32'b8 to reg2
     assign sel_alu_src2[2] = inst_jal;
@@ -259,7 +260,7 @@ module ID(
     assign op_sub = inst_subu | inst_sub;
     assign op_slt = inst_slt | inst_slti;
     assign op_sltu = inst_sltu | inst_sltiu;
-    assign op_and = 1'b0;
+    assign op_and = inst_and | inst_andi;
     assign op_nor = 1'b0;
     assign op_or = inst_ori | inst_or;
     assign op_xor = inst_xor;
@@ -290,15 +291,15 @@ module ID(
     // regfile store enable
     assign rf_we = inst_ori | inst_lui | inst_addiu | inst_subu | inst_jal | inst_addu | inst_sll | inst_or 
                  | inst_lw | inst_xor | inst_sltu | inst_slt | inst_slti | inst_sltiu | inst_add | inst_addi
-                 | inst_sub;
+                 | inst_sub | inst_and | inst_andi;
 
 
 
     // store in [rd]
     assign sel_rf_dst[0] = inst_addu | inst_subu | inst_sll | inst_or | inst_xor | inst_sltu | inst_slt | inst_add
-                         | inst_sub;
+                         | inst_sub | inst_and;
     // store in [rt] 
-    assign sel_rf_dst[1] = inst_ori | inst_lui | inst_addiu | inst_lw | inst_slti | inst_sltiu | inst_addi;
+    assign sel_rf_dst[1] = inst_ori | inst_lui | inst_addiu | inst_lw | inst_slti | inst_sltiu | inst_addi | inst_andi;
     // store in [31]
     assign sel_rf_dst[2] = inst_jal;
 
