810157c331d7f0e5fa4e167638ada3f774cfcbdf
fluctlight001 <609804988@qq.com>
2021-11-26 19:38:41 +0800

    add module : add.v fa.v mul.v

--- EX.v
+++ EX.v
@@ -126,17 +126,17 @@ module EX(
     assign hi_we = inst_div | inst_divu | inst_mult | inst_multu | inst_mthi;
     assign lo_we = inst_div | inst_divu | inst_mult | inst_multu | inst_mtlo;
 
-    assign hi_o = inst_div ? (rf_rdata1[31] ? {rf_rdata1[31],~mod_result[30:0]}+1'b1 : {rf_rdata1[31],mod_result[30:0]}) :
+    assign hi_o = inst_mthi ? rf_rdata1 :
+                  inst_div ? (rf_rdata1[31] ? {rf_rdata1[31],~mod_result[30:0]}+1'b1 : {rf_rdata1[31],mod_result[30:0]}) :
                   inst_divu ? mod_result :
                   inst_mult ? mult_result[63:32] :
                   inst_multu ? multu_result[63:32] :
-                  inst_mthi ? rf_rdata1 :
                   32'b0;
-    assign lo_o = inst_div ? (sign_flag ? {sign_flag,~div_result[30:0]}+1'b1 : {sign_flag,div_result[30:0]}):
+    assign lo_o = inst_mtlo ? rf_rdata1 :
+                  inst_div ? (sign_flag ? {sign_flag,~div_result[30:0]}+1'b1 : {sign_flag,div_result[30:0]}):
                   inst_divu ? div_result :
                   inst_mult ? mult_result[31:0] :
                   inst_multu ? multu_result[31:0] :
-                  inst_mtlo ? rf_rdata1 :
                   32'b0;
 
     assign hilo_bus = {
